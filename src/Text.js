042441d53a28d5ea2d89ef4efa594f170a556f73
lalitha <20b01a1260@svecw.edu.in>
2022-02-08 19:30:55 +0530

    this is my react file

---
+++ src/Text.js
@@ -0,0 +1,25 @@
+import Lizard from './images/lizard.jpeg';
+
+function Text()
+
+{
+    return(
+           <div>
+            <div class="box" >
+            <img src={Lizard}  />  
+            <div class="inside">
+
+            
+           <h1>Lizard</h1>
+          <p>
+               Lizards are wide spread group of squamate
+           reptiles,with over 6000 species ,ranging across
+           all continents except Antartica.</p>
+           <pre> SHARE     LEARN MORE</pre>
+          
+           </div>
+         </div>
+         </div>
+    )
+}
+export default Text;
---
+++ src/index.js
@@ -0,0 +1,25 @@
+
+import React from "react";
+import ReactDOM from "react-dom";
+import './styles.css';
+import  Text from './Text.js';
+// import Lizard from './images/lizard.jpeg';
+
+//
+//   function Image()
+//  {
+//      return(
+//          <img src={Lizard} />
+//      )
+//  }
+
+
+ReactDOM.render(
+    <>
+  
+    {/* <Image /> */}
+    <Text />
+    </>,
+
+     document.getElementById("root")
+);
